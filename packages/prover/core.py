59043fdc285d312dadb208e8a8959f7a58db6131
SoraSuegami <suegamisora@gmail.com>
2024-10-04 15:32:03 +0900

    Fix prover

--- packages/prover/core.py
+++ packages/prover/core.py
@@ -28,9 +28,10 @@ def store_input(circuit_name: str, nonce: str, json_data: dict):
     json_file_path = os.path.join(
         build_dir, "input_" + circuit_name + "_" + nonce + ".json"
     )
+    print(json_file_path)
     logger.info(f"Store user input to {json_file_path}")
     with open(json_file_path, "w") as json_file:
-        json_file.write(json_data)
+        json_file.write(json.dumps(json_data))
 
 
 def load_proof(circuit_name: str, nonce: str) -> dict:
