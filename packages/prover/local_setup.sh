937e5abb8a1c60bccdac1ec7f822330894216e88
SoraSuegami <suegamisora@gmail.com>
2024-10-07 15:10:18 +0900

    Upload Update prover and verifier

--- packages/prover/local_setup.sh
+++ packages/prover/local_setup.sh
@@ -2,13 +2,16 @@
 set -e # Stop on error
 
 mkdir -p build
-
+mkdir -p params
 npm install -g snarkjs@latest
 pip install -r requirements.txt
-mkdir build && cd build
-gdown "https://drive.google.com/uc?id=1vpXh7w2YRzYK1rNdoKAI4Zu857MFml_R"
-unzip upload-file.zip
-mv upload-file/* params
+cd params
+curl https://storage.googleapis.com/zk-jwt-params/jwt-verifier.zkey --output ./jwt-verifier.zkey
+curl https://storage.googleapis.com/zk-jwt-params/jwt-verifier_js/jwt-verifier.wasm --output ./jwt-verifier.wasm
+cd ../
+# gdown "https://drive.google.com/uc?id=1vpXh7w2YRzYK1rNdoKAI4Zu857MFml_R"
+# unzip upload-file.zip
+# mv upload-file/* params
 # curl https://email-wallet-trusted-setup-ceremony-pse-p0tion-production.s3.eu-central-1.amazonaws.com/circuits/emailwallet-account-creation/contributions/emailwallet-account-creation_00019.zkey --output /root/params/account_creation.zkey
 # curl https://email-wallet-trusted-setup-ceremony-pse-p0tion-production.s3.eu-central-1.amazonaws.com/circuits/emailwallet-account-init/contributions/emailwallet-account-init_00007.zkey --output /root/params/account_init.zkey
 # curl https://email-wallet-trusted-setup-ceremony-pse-p0tion-production.s3.eu-central-1.amazonaws.com/circuits/emailwallet-account-transport/contributions/emailwallet-account-transport_00005.zkey --output /root/params/account_transport.zkey
