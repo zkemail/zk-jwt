d31bb35e9e2a65e403e97c4171c16cdb93ca53c2
wshino <wshinohara@gmail.com>
2024-10-13 01:07:42 +0900

    Add Deploy_jwtVerifier.s.sol

--- packages/contracts/script/Deploy_jwtVerifier.s.sol
+++ packages/contracts/script/Deploy_jwtVerifier.s.sol
@@ -4,20 +4,37 @@ pragma solidity ^0.8.12;
 
 import "forge-std/Script.sol";
 import "../src/utils/JwtVerifier.sol";
+import "../src/utils/JwtGroth16Verifier.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 // 1. `source .env`
 // 2. `forge script script/Deploy_jwtVerifier.s.sol:DeployScript --rpc-url $RPC_URL --verify --etherscan-api-key $ETHERSCAN_API_KEY --broadcast -vvvv`
 contract DeployScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        address deployer = vm.addr(deployerPrivateKey);
+        address initialOwner = deployer;
         bytes32 salt = keccak256(abi.encodePacked(vm.envString("DEPLOY_SALT")));
 
         vm.startBroadcast(deployerPrivateKey);
 
         // Deploy the contract using CREATE2
-        JwtVerifier jwtVerifier = new JwtVerifier{salt: salt}();
+        JwtVerifier jwtVerifierImpl = new JwtVerifier();
+        console.log(
+            "JWTVerifier implementation deployed at: %s",
+            address(jwtVerifierImpl)
+        );
+        JwtGroth16Verifier groth16Verifier = new JwtGroth16Verifier();
+        ERC1967Proxy jwtVerifierProxy = new ERC1967Proxy{salt: salt}(
+            address(jwtVerifierImpl),
+            abi.encodeCall(
+                jwtVerifierImpl.initialize,
+                (initialOwner, address(groth16Verifier))
+            )
+        );
 
-        console.log("Contract deployed to:", address(jwtVerifier));
+        JwtVerifier jwtVerifier = JwtVerifier(address(jwtVerifierProxy));
+        console.log("JWTVerifier proxy deployed to:", address(jwtVerifier));
 
         vm.stopBroadcast();
     }
